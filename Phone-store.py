cb927beaa5e352553794da62ef53d48f31d7ef97
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-11-16 22:29:18 +0100

    Create Phone-store.py

---
+++ Phone-store.py
@@ -0,0 +1,20 @@
+class mobile () :
+  def _init_(self, company_name, storage, serial_num, name, dual_sim, support_4k) :
+    self.company_name = company_name
+    self.storage = storage
+    self.serial_num = serial_num
+    self.name = name
+    self.dual_sim = dual_sim
+    self.support_4k = support_4k
+ 
+  def info (self) :
+    print("Phone characteristics: \n company : {} \n storage : {} \n serial_name : {} \n dual_sim : {} \n support_4k : {}")
+
+
+
+  
+  
+  
+  
+  
+  
